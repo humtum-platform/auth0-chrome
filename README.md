0551a8bb70582e776172cdd843499d4a6410390c
linhhoang <lthoang@wpi.edu>
2020-05-18 22:59:18 -0400

    add refresh implementation and more secured pkce

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
-# Auth0 for Chrome extensions [DEPRECATED]
+# Auth0 for Chrome extensions [Forked for Humtum]
 [![All Contributors](https://img.shields.io/badge/all_contributors-3-orange.svg?style=flat-square)](#contributors)
 <img src="https://img.shields.io/badge/community-driven-brightgreen.svg"/> <br>
 
-### Deprecation notice
-
-This repository has been deprecated.
+### Changelog
+* Refresh token implementation
 
--- src/PKCEClient.js
+++ src/PKCEClient.js
@@ -1,7 +1,10 @@
 import '@babel/runtime/regenerator'
 import generateRandomChallengePair from './generateRandomChallengePair';
 import parse from 'url-parse';
-import { boundMethod } from 'autobind-decorator'
+import {
+  boundMethod
+} from 'autobind-decorator'
+import cryptoRandomString from 'crypto-random-string'
 
 const qs = parse.qs;
 /*
@@ -10,24 +13,28 @@ const qs = parse.qs;
   OAuth flows (like Windows?)
 */
 
-class PKCEClient{
+class PKCEClient {
   // These params will never change
-  constructor (domain, clientId) {
+  constructor(domain, clientId) {
     this.domain = domain;
     this.clientId = clientId;
   }
 
-  async getAuthResult (url, interactive) {
+  async getAuthResult(url, interactive) {
     throw new Error('Must be implemented by a sub-class');
   }
 
-  getRedirectURL () {
+  getRedirectURL() {
     throw new Error('Must be implemented by a sub-class');
   }
 
   @boundMethod
-  async exchangeCodeForToken (code, verifier) {
-    const {domain, clientId} = this;
+  async exchangeCodeForToken(code, verifier, estate) {
+    const {
+      domain,
+      clientId
+    } = this;
+    if (localStorage.authzeroState !== estate) throw Error("state does not match")
     const body = JSON.stringify({
       redirect_uri: this.getRedirectURL(),
       grant_type: 'authorization_code',
@@ -38,44 +45,86 @@ class PKCEClient{
     const result = await fetch(`https://${domain}/oauth/token`, {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
       body
     });
 
-    if(result.ok)
-     return result.json();
+    if (result.ok)
+      return result.json();
 
     throw Error(result.statusText);
   }
 
-  extractCode (resultUrl) {
+  @boundMethod
+  async refreshToken(refreshToken) {
+    const {
+      domain,
+      clientId
+    } = this;
+    if (refreshToken) {
+
+      const result = await fetch(`https://${domain}/oauth/token`, {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify({
+          grant_type: 'refresh_token',
+          client_id: clientId,
+          refresh_token: refreshToken,
+        })
+      });
+      if (result.ok)
+        return result.json();
+
+      throw Error(result.statusText);
+    } else throw Error("refresh_token is empty")
+  }
+
+  extractCode(resultUrl) {
     const response = parse(resultUrl, true).query;
 
     if (response.error) {
       throw new Error(response.error_description || response.error);
     }
 
-    return response.code;
+    return {
+      code: response.code,
+      state: response.state
+    };
   }
 
   @boundMethod
-  async authenticate (options = {}, interactive = true) {
-    const {domain, clientId} = this;
-    const {secret, hashed} = generateRandomChallengePair();
+  async authenticate(options = {}, interactive = true) {
+    const {
+      domain,
+      clientId
+    } = this;
+    const {
+      secret,
+      hashed
+    } = generateRandomChallengePair();
+    localStorage.authzeroState = cryptoRandomString({
+      length: 10
+    });
 
-    Object.assign(options, {
+    const authOptions = Object.assign({}, options, {
       client_id: clientId,
       code_challenge: hashed,
       redirect_uri: this.getRedirectURL(),
       code_challenge_method: 'S256',
       response_type: 'code',
+      state: localStorage.authzeroState
     });
 
-    const url = `https://${domain}/authorize?${qs.stringify(options)}`;
+    const url = `https://${domain}/authorize?${qs.stringify(authOptions)}`;
     const resultUrl = await this.getAuthResult(url, interactive);
-    const code = this.extractCode(resultUrl);
-    return this.exchangeCodeForToken(code, secret);
+    const {
+      code,
+      state
+    } = this.extractCode(resultUrl);
+    return this.exchangeCodeForToken(code, secret, state);
   }
 }
 
